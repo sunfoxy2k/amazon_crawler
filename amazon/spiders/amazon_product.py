2516881cbab537addbcbf03048a5169d77bffed9
SunFoxY2K <phuc1012000@gmail.com>
2022-03-19 15:33:39 +0700

    submit

--- amazon/spiders/amazon_product.py
+++ amazon/spiders/amazon_product.py
@@ -1,20 +1,29 @@
 import scrapy
+import re
 from urllib.parse import urljoin
+from bs4 import BeautifulSoup
 
 
-def parse_product(response):
+def parse_product(self,response):
     all_comments = response.css(
         '#reviews-medley-footer .a-link-emphasis.a-text-bold::attr(href)').get()
 
     if all_comments:
         all_comments = urljoin('https://www.amazon.com/', all_comments)
 
-    asin = response.url.split('/dp/')[1].split('/')[0]
+    if '/dp/' in response.url:
+        asin = response.url.split('/dp/')[1].split('/')[0]
+    elif '/gp/product/' in response.url:
+        asin = response.url.split('/gp/product/')[1].split('/')[0]
+    else:
+        asin = None
+
     category = response.css(
         '.a-unordered-list.a-horizontal.a-size-small li>span>a::text').getall()
     category = [val.strip() for val in category]
     category = category[-1] if len(category) > 0 else ''
-
+    from scrapy.shell import inspect_response
+    inspect_response(response, self)
     total_rating = response.css(
         '.averageStarRatingNumerical .a-color-secondary::text').get().strip().replace(
         ' global ratings', '')
@@ -24,17 +33,7 @@ def parse_product(response):
     rating = [total_rating * int(val[18:].replace('% of rating', '')) // 100 for val in
               rating]
 
-    description = response.css('.aplus-v2.desktop.celwidget').xpath('//p//text()') \
-        .getall()
-    description = response.css('#productDescription span::text').getall() \
-        if len(description) == 0 else description
-
-    description = [
-        val.strip() for val in description if
-        len(val) < 10 and val != ' Click to play video '
-    ]
-
-    description = ''.join(description)
+    description = response.css('#productDescription > p::text').get()
 
     title = response.css('#productTitle::text').get()
     title = title.strip() if title else None
@@ -47,8 +46,24 @@ def parse_product(response):
     if price is not None:
         price = float(price.replace('$', ''))
 
-    print(asin)
-    return {
+    overall_rating = response.css('#reviewsMedley .a-size-medium::text').extract_first()
+    overall_rating = overall_rating.split()[0]
+
+    star_rating_list = response.css('#histogramTable .a-link-normal::text').extract()
+    star_rating = {}
+    star_rating_list = list(map(lambda k: k.strip(), star_rating_list))
+    star_rating_list = list(filter(None, star_rating_list))
+    for i in range(len(star_rating_list)):
+        if i % 2 == 1:
+            continue
+        star_rating[star_rating_list[i]] = star_rating_list[i + 1]
+    for i in range(5, 0, -1):
+        if star_rating.get(str(i) + " star", None) == None:
+            star_rating[str(i) + " star"] = "0%"
+    star_rating = dict(sorted(star_rating.items()))
+
+
+    data = {
         'asin': asin,
         'category': category,
         'description': description,
@@ -62,8 +77,20 @@ def parse_product(response):
         'stars_3': rating[2],
         'stars_2': rating[3],
         'stars_1': rating[4],
+        'global_rating': total_rating,
+        'overall_rating': overall_rating,
+        'star_rating': star_rating,
     }
 
+    if asin != None:
+        url = "https://www.amazon.com/hz/reviews-render/ajax/lazy-widgets/stream?asin=" + asin + "&language=en_US&lazyWidget=cr-summarization-attributes"
+        yield scrapy.Request(
+        url,
+        meta={'data': data},
+            callback=self.parse_feature_rating
+    )
+    else:
+        yield data
 
 class AmazonProductSpider(scrapy.Spider):
     name = 'amazon_product'
@@ -75,11 +102,23 @@ class AmazonProductSpider(scrapy.Spider):
         }
     }
 
-    def __init__(self, limit, urls, **kwargs):
+    def __init__(self, limit=1, urls=['https://www.amazon.com/gp/product/B08Y9891ZD/ref=ox_sc_saved_image_8?smid=ATVPDKIKX0DER&psc=1'], **kwargs):
         self.custom_settings['DEPTH_LIMIT'] = limit
         self.start_urls = urls
 
         super().__init__(**kwargs)
 
     def parse(self, response, **kwargs):
-        return parse_product(response)
+        return parse_product(self,response)
+
+    def parse_feature_rating(self, response):
+        data = response.meta.get('data')
+        html_text = response.body.decode('utf-8').replace('"', '').replace("\\n", "").replace("\\", "").split(',')
+        soup = BeautifulSoup(html_text[2], 'html.parser')
+        feature_list = soup.find_all(id=re.compile("cr-summarization-attribute-attr-"))
+        rating = {}
+        for i in range(len(feature_list)):
+            feature_set = feature_list[i].find_all("span", {"class": "a-size-base"})
+            rating[feature_set[0].text] = feature_set[1].text
+        data["feature_rating"] = rating
+        yield data
